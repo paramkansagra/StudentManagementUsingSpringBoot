90913385bcd6456d222cbe47c4b49e2fb2294ecc
paramkansagra <param.j.kansagra@gmail.com>
2025-12-22 19:48:26 +0530

    Added controller

--- src/main/java/com/paramkansagra/StudentManagement/controller/StudentController.java
+++ src/main/java/com/paramkansagra/StudentManagement/controller/StudentController.java
@@ -1,21 +1,29 @@
 package com.paramkansagra.StudentManagement.controller;
 
 import com.paramkansagra.StudentManagement.dto.StudentDTO;
-import com.paramkansagra.StudentManagement.entity.Student;
+import com.paramkansagra.StudentManagement.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping("/students")
-    public StudentDTO getStudents(){
-        return new StudentDTO(1L , "Param Kansagra" , "someRandomEmail@gmail.com");
+    public List<StudentDTO> getStudents() {
+        return studentService.getStudents();
     }
 
     @GetMapping("/students/{id}")
-    public String getStudentByID(@PathVariable Long id){
-        return "Getting student with id -> " + id;
+    public StudentDTO getStudentByID(@PathVariable Long id) {
+        return studentService.getStudentByID(id);
     }
 }
--- src/main/java/com/paramkansagra/StudentManagement/entity/Student.java
+++ src/main/java/com/paramkansagra/StudentManagement/entity/Student.java
@@ -1,14 +1,19 @@
 package com.paramkansagra.StudentManagement.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
+@Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
 }
